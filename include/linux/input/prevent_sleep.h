a51d05eee85870562f64945945e448cfd5884071
ED300 <emadmelk300@yahoo.com>
2017-04-27 21:38:49 +0200

    wt86518: update

--- include/linux/input/prevent_sleep.h
+++ include/linux/input/prevent_sleep.h
@@ -31,7 +31,7 @@ extern bool prevent_sleep;
 
 #if defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE) && defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE)
 #define ts_get_prevent_sleep(prevent_sleep) { \
-	prevent_sleep = (s2w_switch || dt2w_switch); \
+	prevent_sleep = (s2w_switch > 0 || dt2w_switch > 0); \
 }
 #else
 #if defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE) || defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE)
